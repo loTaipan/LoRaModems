c57a15c832ce4c3cc680bb31061ef6930b4f88bb
morgulbrut <tillo.bosshart@gmail.com>
2016-06-02 01:27:45 +0200

    default datarate change

--- microchip_lora_modem.py
+++ microchip_lora_modem.py
@@ -5,6 +5,11 @@ uart_lora = pyb.UART(3, 57600)
 
 debug = True
 
+'''TODO:
+- OTAA
+- Recieve downlink 
+
+'''
 
 def send_command(cmd):
     if debug:
@@ -12,16 +17,12 @@ def send_command(cmd):
     uart_lora.write(cmd + '\r\n')
     uart_read(uart_lora)
 
-
 ''' Sets the network session and the appkey, default thethingsnetwork'''
-
-
 def set_network_settings(nwkskey='2B7E151628AED2A6ABF7158809CF4F3C',
                          appskey='2B7E151628AED2A6ABF7158809CF4F3C'):
     send_command('mac set nwkskey ' + nwkskey)
     send_command('mac set appskey' + appskey)
     send_command('mac save')
-
 
 ''' Sets the device adress'''
 def set_device_adress(addr):
@@ -29,11 +30,11 @@ def set_device_adress(addr):
     send_command('mac save')
 
 ''' indx 1 = 14dBm'''
-def set_output_power(indx):
+def set_output_power(indx='1'):
     send_command('mac set pwridx' + indx)
     send_command('mac save')
 
-def connect_lora(datarate='2'):
+def connect(datarate='4'):
     send_command('mac join abp')
     send_command('mac set dr ' + datarate)
     send_command('mac set adr on')
@@ -42,7 +43,6 @@ def connect_lora(datarate='2'):
 def send_message(message, confirmation='uncnf', port='1'):
     msg = binascii.hexlify(message).decode('utf-8')
     send_command('mac tx ' + confirmation + ' ' + port + ' ' + msg)
-
 
 def uart_read(uart, ok_raise=False):
     msg = ''
