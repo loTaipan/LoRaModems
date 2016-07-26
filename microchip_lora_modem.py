26962c274f25bd4fc087a08eb85672df366fb725
Tillo Bosshart <bosshart@miromico.ch>
2016-07-26 13:01:17 +0200

    OTAA implemented

--- microchip_lora_modem.py
+++ microchip_lora_modem.py
@@ -6,7 +6,6 @@ uart_lora = pyb.UART(3, 57600)
 debug = True
 
 '''TODO:
-- OTAA
 - Recieve downlink 
 
 '''
@@ -17,27 +16,29 @@ def send_command(cmd):
     uart_lora.write(cmd + '\r\n')
     uart_read(uart_lora)
 
-''' Sets the network session and the appkey, default thethingsnetwork'''
-def set_network_settings(nwkskey='2B7E151628AED2A6ABF7158809CF4F3C',
-                         appskey='2B7E151628AED2A6ABF7158809CF4F3C'):
+''' Sets the credentials for ABP'''
+def set_network_settings_ABP(devaddr, nwkskey,appskey):
+	send_command('mac set devaddr ' + devaddr)
     send_command('mac set nwkskey ' + nwkskey)
     send_command('mac set appskey' + appskey)
     send_command('mac save')
 
-''' Sets the device adress'''
-def set_device_adress(addr):
-    send_command('mac set devaddr ' + addr)
-    send_command('mac save')
+''' Sets the credentials for OTAA'''	
+def set_network_settings_OTAA(deveui, appeui):
+	send_command('mac set deveui ' + deveui)
+    send_command('mac set appeui ' + appeui)
+    send_command('mac save')	
 
 ''' indx 1 = 14dBm'''
 def set_output_power(indx='1'):
     send_command('mac set pwridx' + indx)
     send_command('mac save')
 
-def connect(datarate='4'):
-    send_command('mac join abp')
+		
+def connect(datarate='0', mode ='abp', adr = 'off'):
+    send_command('mac join ' + mode)
     send_command('mac set dr ' + datarate)
-    send_command('mac set adr on')
+    send_command('mac set adr ' + adr)
 
 
 def send_message(message, confirmation='uncnf', port='1'):
