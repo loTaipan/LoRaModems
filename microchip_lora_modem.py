b482cf7cbf0af1c4cb29cad2d9c5d74aeb48cfeb
morgulbrut <tillo.bosshart@gmail.com>
2016-09-13 23:16:54 +0200

    fix in set_network_settings_ABP

--- microchip_lora_modem.py
+++ microchip_lora_modem.py
@@ -16,16 +16,16 @@ def send_command(cmd):
 
 ''' Sets the credentials for ABP'''
 def set_network_settings_ABP(devaddr, nwkskey,appskey):
-	send_command('mac set devaddr ' + devaddr)
+    send_command('mac set devaddr ' + devaddr)
     send_command('mac set nwkskey ' + nwkskey)
-    send_command('mac set appskey' + appskey)
+    send_command('mac set appskey ' + appskey)
     send_command('mac save')
 
-''' Sets the credentials for OTAA'''	
+''' Sets the credentials for OTAA'''
 def set_network_settings_OTAA(deveui, appeui):
-	send_command('mac set deveui ' + deveui)
+    send_command('mac set deveui ' + deveui)
     send_command('mac set appeui ' + appeui)
-    send_command('mac save')	
+    send_command('mac save')
 
 ''' indx 1 = 14dBm'''
 def set_output_power(indx='1'):
@@ -34,8 +34,8 @@ def set_output_power(indx='1'):
 
 def reset():
     send_command('sys reset')
-	
-def connect(datarate='0', mode ='abp', adr = 'off'):
+
+def connect(datarate='3', mode ='abp', adr = 'off'):
     send_command('mac join ' + mode)
     send_command('mac set dr ' + datarate)
     send_command('mac set adr ' + adr)
@@ -45,11 +45,14 @@ def send_message(message, confirmation='uncnf', port='1'):
     send_command('mac tx ' + confirmation + ' ' + port + ' ' + msg)
 
 def send_message_raw(message, confirmation='uncnf', port='1'):
-    send_command('mac tx ' + confirmation + ' ' + port + ' ' + msg)
+    send_command('mac tx ' + confirmation + ' ' + port + ' ' + message)
+
+def sleep(time):
+    send_command('sys sleep ' + str(time))
 
 def process_downlink(message):
-	pass
-	
+    pass
+
 def uart_read(uart, ok_raise=False):
     msg = ''
     attempts = 10
@@ -61,11 +64,11 @@ def uart_read(uart, ok_raise=False):
                 if debug:
                     print(msg)
                 break
-			if 'mac rx' in msg:
-				if debug:
-					print(msg)
-				break
-				process_downlink(msg)
+            if 'mac rx' in msg:
+                if debug:
+                    print(msg)
+                break
+            process_downlink(msg)
         attempts -= 1
     if attempts < 0 and ok_raise:
         raise Exception('no OK received!')
